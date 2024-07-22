3ffd77e0b2f4a5d78bfa8692bf703c6e70ce12f1
Riccardo Padoan <riccardo.padoan@exb.de>
2024-07-22 09:21:19 +0100

    feat: migrate to copier (`betaboon/copier-python`)

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    pdm
+    ruff
+    # required for taplo-pre-commit
+    cargo
+  ];
+
+  shellHook = ''
+    # dirty hack to get ruff-pre-commit working
+    ruff_bin=$(find ~/.cache/pre-commit -type f -name ruff)
+    [ -f "$ruff_bin" ] && ln -sf ${pkgs.ruff}/bin/ruff $ruff_bin
+
+    # dirty hack to get ruff in venv working
+    ruff_bin=$(find .venv -type f -name ruff)
+    [ -f "$ruff_bin" ] && ln -sf ${pkgs.ruff}/bin/ruff $ruff_bin
+  '';
+}
